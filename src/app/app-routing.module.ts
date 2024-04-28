768640695baa5212a8468eab129ee40f075574f2
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-04-28 16:28:48 -0500

    feat(suscription): added suscription component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LawyerListComponent} from "./public/pages/lawyer-list/lawyer-list.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {SuscriptionsComponent} from "./public/pages/suscriptions/suscriptions.component";
 
 const routes: Routes = [
   { path: 'abogados', component: LawyerListComponent },
   { path: '', redirectTo: 'abogados', pathMatch: 'full'},
+  { path: 'suscripcion', component: SuscriptionsComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 
