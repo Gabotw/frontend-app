23a218d7e3e02008e791be4709815f5943301a45
Gabo <u202018627@upc.edu.pe>
2024-06-18 09:06:13 -0500

    feat: added resource services.

---
+++ src/app/educational-resource/services/resource.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Resource} from "../model/resource.entity";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResourceService extends BaseService<Resource>{
+  constructor(http:HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/educational-resources';
+  }
+}
