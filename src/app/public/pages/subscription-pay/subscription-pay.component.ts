a1097043c6660dc32c43f93e20cb767601add699
Gabotw <u202018627@upc.edu.pe>
2024-06-21 17:38:50 -0500

    fix: fixed minor problems.

--- src/app/public/pages/subscription-pay/subscription-pay.component.ts
+++ src/app/public/pages/subscription-pay/subscription-pay.component.ts
@@ -4,7 +4,6 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 import {InformationPayComponent} from "../information-pay/information-pay.component";
 import {Lawyer} from "../../../lawyers/model/lawyer.entity";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-subscription-pay',
