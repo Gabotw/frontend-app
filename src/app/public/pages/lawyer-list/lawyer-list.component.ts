b840ac6556ba9ed73e5dd50810d709ee3bba7001
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-06-21 17:51:57 -0500

    feat: updated lawyer-list.

--- src/app/public/pages/lawyer-list/lawyer-list.component.ts
+++ src/app/public/pages/lawyer-list/lawyer-list.component.ts
@@ -4,6 +4,8 @@ import {LawyerService} from "../../../lawyers/services/lawyer.service";
 import { LawyerProfileComponent } from '../../../lawyers/pages/lawyer-profile/lawyer-profile.component';
 import { MatDialog} from "@angular/material/dialog";
 import {LawyerFilterComponent} from "../lawyer-filter/lawyer-filter.component";
+import {Profile} from "../../../profile/model/profile.entity";
+import {ProfileService} from "../../../profile/services/profile.service";
 
 @Component({
   selector: 'app-lawyer-list',
@@ -11,19 +13,44 @@ import {LawyerFilterComponent} from "../lawyer-filter/lawyer-filter.component";
   styleUrls: ['./lawyer-list.component.css']
 })
 export class LawyerListComponent implements OnInit {
-  lawyers: Lawyer[] = [];
+  lawyers: any[] = [];
+  profiles: Profile[] = [];
+
   filteredLawyers: Lawyer[] = [];
   searchTerm: string = '';
 
-  constructor(private lawyerService: LawyerService, public dialog: MatDialog) { }
+  constructor(
+    private lawyerService: LawyerService,
+    private profileService: ProfileService,
+    public dialog: MatDialog
+  )  { }
 
   ngOnInit(): void {
-    this.lawyerService.getAll().subscribe((lawyers) => {
+    /*this.lawyerService.getAll().subscribe((lawyers) => {
       this.lawyers = lawyers;
       this.filteredLawyers = lawyers;
+    });*/
+
+    this.profileService.getAllProfiles().subscribe((profiles) => {
+      this.profiles = profiles;
+
+      this.lawyerService.getAllLawyers().subscribe((lawyers) => {
+        this.lawyers = lawyers.map(lawyer => this.processLawyerData(lawyer));
+      });
     });
   }
-  search(): void {
+  private processLawyerData(lawyer: Lawyer): any {
+    const profile = this.profiles.find(p => p.id === lawyer.profileId);
+
+    return {
+      ...lawyer,
+      lawyerName: profile ? profile.fullName : '',
+      email: profile ? profile.email : '',
+      phoneNumber: profile ? profile.phoneNumber : ''
+    };
+  }
+
+  /*search(): void {
     if (this.searchTerm) {
       this.filteredLawyers = this.lawyers.filter(lawyer =>
         lawyer.name.toLowerCase().includes(this.searchTerm.toLowerCase())
@@ -31,9 +58,9 @@ export class LawyerListComponent implements OnInit {
     } else {
       this.filteredLawyers = this.lawyers;
     }
-  }
+  }*/
 
-  openDialog(lawyerId: number){
+  openDialog(lawyerId: string){
     const dialogRef = this.dialog.open(LawyerProfileComponent, {
       data: { id: lawyerId }
     });
