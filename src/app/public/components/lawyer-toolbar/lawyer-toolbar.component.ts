6b7dc9ea9b46d1151b75bf7839453d850a9dbcf2
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-06-23 15:32:48 -0500

    feat(lawyer-view): added lawyer toolbar and lawyer view.

---
+++ src/app/public/components/lawyer-toolbar/lawyer-toolbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lawyer-toolbar',
+  templateUrl: './lawyer-toolbar.component.html',
+  styleUrl: './lawyer-toolbar.component.css'
+})
+export class LawyerToolbarComponent {
+
+}
