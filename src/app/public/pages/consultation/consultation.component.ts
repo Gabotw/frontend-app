1bbfc83ee1effb3dd117e7c6bb245a74f0805579
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-06-24 17:50:29 -0500

    feat(summary-section): updated consultation component.

--- src/app/public/pages/consultation/consultation.component.ts
+++ src/app/public/pages/consultation/consultation.component.ts
@@ -1,18 +1,48 @@
-import { Inject, Component } from '@angular/core';
+import {Inject, Component, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Lawyer} from "../../../lawyers/model/lawyer.entity";
+import {Profile} from "../../../profile/model/profile.entity";
+import {LawyerService} from "../../../lawyers/services/lawyer.service";
+import {ProfileService} from "../../../profile/services/profile.service";
 
 @Component({
   selector: 'app-consultation',
   templateUrl: './consultation.component.html',
   styleUrl: './consultation.component.css'
 })
-export class ConsultationComponent {
-
+export class ConsultationComponent implements OnInit {
+  lawyer: any;
+  profile: any;
+  lawyers: any[] = [];
+  profiles: Profile[] = [];
   constructor(
     public dialogRef: MatDialogRef<ConsultationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { lawyer: Lawyer }
-  ) {}
+    private lawyerService: LawyerService,
+    private profileService: ProfileService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {  this.lawyer = data.lawyer; }
+
+  ngOnInit(): void {
+
+    this.profileService.getAllProfiles().subscribe((profiles) => {
+      this.profiles = profiles;
+
+      this.lawyerService.getAllLawyers().subscribe((lawyers) => {
+        this.lawyers = lawyers.map(lawyer => this.processLawyerData(lawyer));
+      });
+    });
+  }
+
+  private processLawyerData(lawyer: Lawyer): any {
+    const profile = this.profiles.find(p => p.id === lawyer.profileId);
+
+    return {
+      ...lawyer,
+      lawyerName: profile ? profile.fullName : '',
+      email: profile ? profile.email : '',
+      phoneNumber: profile ? profile.phoneNumber : ''
+    };
+  }
 
   closeDialog(): void {
     this.dialogRef.close();
